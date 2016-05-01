61e8f4b81cc91d40925b711de1beba865124479e
Martin Tsang <m4rtin.t@gmail.com>
2016-05-01 13:00:35 -0700

    Timer Change

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -15,7 +15,9 @@ public class Timer : MonoBehaviour {
 	{
 		timeLeft -= Time.deltaTime;
 
-		this.GetComponent<Text> ().text = ""+Mathf.Round(timeLeft * 100f) / 100f;
+		this.GetComponent<Text> ().text = string.Format("{0:0.00}", Mathf.Round(timeLeft * 100.0f) / 100.0f);
+			//""+Mathf.Round(timeLeft * 100f) / 100f;
+
 		if(timeLeft < 0)
 		{
 			//Debug.Log ("Finished");
