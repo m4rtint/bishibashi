2374cc64f7c99ceafa9dead82539bd0856876e59
Martin <m4rtin.t@gmail.com>
2016-05-01 02:11:42 -0700

    initial commit for jumping girl game

---
+++ Assets/scripts/Player_Control.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Control : MonoBehaviour {
+	public bool left = false;
+	public bool right = false;
+	public bool down = false;
+
+	public float last_x_position = -1;
+
+	Animator anim;
+
+	GameObject global;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+		last_x_position = 3;
+		global = GameObject.Find ("TextScore");
+	}
+
+	void Update () {
+		left = Input.GetKeyUp (KeyCode.LeftArrow);
+		right = Input.GetKeyUp (KeyCode.RightArrow);
+		down = Input.GetKeyUp (KeyCode.DownArrow);
+
+		int position_x = (int) transform.position.x;
+
+		switch (position_x) {
+		case 3:
+			if (down) {
+				moveCharacter ("Rjumpleft", (float)-1);
+			}
+			break;
+		case -1:
+		case 0:
+			if (left) {
+				moveCharacter ("Ljumpleft", (float)-4.75);
+			}
+			if (right) {
+				moveCharacter ("Rjumpright", (float)3);
+			}
+			break;
+		case -4:
+			if (down) {
+				moveCharacter ("Ljumpright", (float)-1);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	void moveCharacter(string animation, float x) {
+		last_x_position = transform.position.x;
+		anim.SetTrigger(animation);
+		global.GetComponent<ScoreScript> ().update_score (x);
+	}
+	
+
+}
---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	float timeLeft = 15.0f;
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+
+		this.GetComponent<Text> ().text = ""+Mathf.Round(timeLeft * 100f) / 100f;
+		if(timeLeft < 0)
+		{
+			//Debug.Log ("Finished");
+		}
+	}
+}
