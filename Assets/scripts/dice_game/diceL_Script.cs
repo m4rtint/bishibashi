4cad3820b00656b2d02bac8b1793daa7cfb0de2c
Martin <m4rtin.t@gmail.com>
2016-05-02 23:29:18 -0700

    Animation for each Dice

--- Assets/scripts/dice_game/diceL_Script.cs
+++ Assets/scripts/dice_game/diceL_Script.cs
@@ -6,7 +6,6 @@ public class diceL_Script : MonoBehaviour {
 	Sprite[] dice;
 	Animator anim;
 	bool space = false;
-	bool ablePress = true;
 
 	int value;
 
@@ -21,34 +20,10 @@ public class diceL_Script : MonoBehaviour {
 	void Update() {
 		space = Input.GetKeyDown (KeyCode.Space);
 
-		if (space && ablePress) {
-			this.GetComponent<Animator> ().enabled = true;
-			ablePress = false;
-
-			anim.SetTrigger ("randomizeDice");
+		if (space && !anim.GetCurrentAnimatorStateInfo (0).IsName ("spin")) {
 			value = diceValue.GetComponent<dice_Randomizer> ().get_value (0);
-		
-			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Dice_1")) {
-				this.GetComponent<Animator> ().enabled = true;
-			}
-
-			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("idle")) {
-				//Invoke ("UpdateSprite", 1.5f);
-				set_ablePress_true();
-				this.GetComponent<Animator> ().enabled = true;
-				this.GetComponent<SpriteRenderer> ().sprite = dice[value+3];
-			}
-		
-
+			anim.SetInteger ("Value", value);
+			anim.SetTrigger ("randomizeDice");
 		}
-	}
-
-	void UpdateSprite() {
-		this.GetComponent<Animator> ().enabled = false;
-		this.GetComponent<SpriteRenderer> ().sprite = dice[value+3];
-	}
-
-	public void set_ablePress_true() {
-		ablePress = true;
 	}
 }
