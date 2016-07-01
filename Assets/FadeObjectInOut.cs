12a571e8a051c86137b39773a64b99bd45248e90
Martin <m4rtin.t@gmail.com>
2016-07-01 16:05:06 -0700

    Changed Start Screen

---
+++ Assets/FadeObjectInOut.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeObjectInOut : MonoBehaviour
+{
+	float duration = 1;
+	float alpha = 0;
+
+
+	public void Update(){
+		lerpAlpha();
+	}
+
+	private void lerpAlpha(){
+		float lerp = Mathf.PingPong (Time.time, duration) / duration;
+
+		alpha = Mathf.Lerp (0, 1, lerp);
+		//renderer.GetComponent<Renderer>().color.a = alpha;
+	}
+
+
+}
