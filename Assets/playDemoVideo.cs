962c7090cd07b636291e6246037d5ece130fafd5
Martin <m4rtin.t@gmail.com>
2016-07-02 20:47:41 -0700

    Dice Demo Video

---
+++ Assets/playDemoVideo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof(AudioSource))]
+public class playDemoVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+
+	void Start(){
+		GetComponent<Renderer> ().material.mainTexture = movie as MovieTexture;
+		GetComponent<AudioSource> ().clip = movie.audioClip;
+	
+			movie.Play();
+		GetComponent<AudioSource> ().Play ();
+	}
+}
